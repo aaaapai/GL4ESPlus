ed07e32ae6ce06261622b837cdcf5ef00d45b512
aaaa派 <89151134+aaaapai@users.noreply.github.com>
2025-03-25 22:52:20 +0800

    Update Android.mk

--- src/main/cpp/Android.mk
+++ src/main/cpp/Android.mk
@@ -193,10 +193,10 @@ LOCAL_C_INCLUDES        := $(LOCAL_PATH)/gl4esplus/shaderconv/include \
                            $(LOCAL_PATH)/gl4esplus/shaderconv/src/gallium/auxiliary \
                            $(LOCAL_PATH)/gl4esplus/shaderconv/src/gallium/include
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
-LOCAL_LDLIBS            := -ldl
+LOCAL_LDLIBS            := -ldl -lc
 LOCAL_CPP_FEATURES      := exceptions
-LOCAL_CPPFLAGS           = -std=gnu++20
-LOCAL_CFLAGS            += -std=gnu2x
+LOCAL_CPPFLAGS           = -std=c++11
+LOCAL_CFLAGS            += -std=c11
 LOCAL_CFLAGS            += -D_LIB
 LOCAL_CFLAGS            += -DNOMINMAX
 LOCAL_CFLAGS            += -D_USE_MATH_DEFINES
@@ -206,6 +206,7 @@ LOCAL_CFLAGS            += -D__STDC_FORMAT_MACROS
 LOCAL_CFLAGS            += -D__STDC_CONSTANT_MACROS
 LOCAL_CFLAGS            += -DUTIL_ARCH_LITTLE_ENDIAN
 LOCAL_CFLAGS            += -DUNIX
+LOCAL_CFLAGS            += -fexceptions
 include $(BUILD_SHARED_LIBRARY)
 
 #include $(CLEAR_VARS)
