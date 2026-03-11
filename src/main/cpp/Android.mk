640919ba3243af4a52e27b1c0605ef64f121d656
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-03-11 23:59:37 +0800

    Update Android.mk

--- src/main/cpp/Android.mk
+++ src/main/cpp/Android.mk
@@ -207,7 +207,7 @@ LOCAL_CFLAGS            += -D__STDC_FORMAT_MACROS
 LOCAL_CFLAGS            += -D__STDC_CONSTANT_MACROS
 LOCAL_CFLAGS            += -DUTIL_ARCH_LITTLE_ENDIAN
 LOCAL_CFLAGS            += -DUNIX -DLINUX -DANDROID -DHAVE_GLES3 -DHAVE_GLES2 -DHAVE_OPENGL
-include $(BUILD_STATIC_LIBRARY)
+include $(BUILD_SHARED_LIBRARY)
 
 # include $(CLEAR_VARS)
 # LOCAL_MODULE            := spirv-cross
@@ -221,7 +221,7 @@ include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := gl4es_plus
-LOCAL_STATIC_LIBRARIES  := shaderconv
+LOCAL_SHARED_LIBRARIES  := shaderconv
 LOCAL_SRC_FILES         := gl4esplus/src/gl/arbconverter.c \
                            gl4esplus/src/gl/arbgenerator.c \
                            gl4esplus/src/gl/arbhelper.c \
